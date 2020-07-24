6cfe410df890e303e5ed51ea6dc0a9c00b7eb463
Lane Shields <lshields@128technology.com>
2020-07-24 16:56:44 +0000

    Bind to port 80 since we're now running container with host networking

--- webapp/run.py
+++ webapp/run.py
@@ -2,4 +2,4 @@ from blaster import factory
 import blaster
 if __name__ == "__main__":
     app = factory.create_app(celery=blaster.celery)
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
