a5f7efa307f1cbbb77d762735f239d9364c4eca8
Lane Shields <lshields@128technology.com>
2020-09-18 18:10:45 +0000

    Removed iso_status_enum table in favor of just writing status as text, added status field to node table, writing iso name as text instead of reference in node table to keep data in case iso is deleted, added timestamps to several tables

--- webapp/blaster/schema.sql
+++ webapp/blaster/schema.sql
@@ -1,24 +1,13 @@
-DROP TABLE IF EXISTS iso_status_enum;
 DROP TABLE IF EXISTS iso;
 DROP TABLE IF EXISTS conductor;
 DROP TABLE IF EXISTS quickstart;
 DROP TABLE IF EXISTS node;
 
-CREATE TABLE iso_status_enum (
-  id INTEGER PRIMARY KEY,
-  status TEXT NOT NULL
-);
-
-INSERT INTO iso_status_enum(id, status) VALUES (1, 'Processing');
-INSERT INTO iso_status_enum(id, status) VALUES (2, 'Ready');
-INSERT INTO iso_status_enum(id, status) VALUES (3, 'Failed');
-
 CREATE TABLE iso (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
-  status_id INTEGER NOT NULL,
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (status_id) REFERENCES iso_status_enum (id)
+  status TEXT NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE conductor (
@@ -37,15 +26,17 @@ CREATE TABLE quickstart (
   asset_id TEXT,
   config TEXT,
   description TEXT,
-  default_quickstart INT DEFAULT 0 NOT NULL
+  default_quickstart INT DEFAULT 0 NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE node (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   identifier TEXT NOT NULL,
   quickstart_id INTEGER,
-  iso_id INTEGER,
+  iso_id TEXT,
+  status TEXT NOT NULL,
   validation_report TEXT,
-  FOREIGN KEY (quickstart_id) REFERENCES quickstart (id),
-  FOREIGN KEY (iso_id) REFERENCES iso (id)
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (quickstart_id) REFERENCES quickstart (id)
 );
