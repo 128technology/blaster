a1c620e404e3f929c34dc4df743deae463573325
Mathias Jeschke <mjeschke@juniper.net>
2021-10-25 15:19:31 +0200

    A helper to synchronize HA SSH keys for OTP

---
+++ helper/sync-ha-ssh-keys/create_pyz.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+tmpfile=$(mktemp -d) || exit 1
+script=$(ls -1 *.py | sed -n '1s/\.py//p')
+ln $script.py $tmpfile/__main__.py
+python3 -m pip install -r requirements.txt --target $tmpfile
+python3 -m zipapp --python "/usr/bin/env python3" --output $script.pyz $tmpfile
+rm -r $tmpfile
+
+sed -n '1,/^base64/p' post-bootstrap.skel > post-bootstrap
+gzip -c9 $script.pyz | base64 >> post-bootstrap
+echo EOF >> post-bootstrap
